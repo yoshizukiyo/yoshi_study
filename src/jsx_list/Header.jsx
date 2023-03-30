fd0bee4e067fd8faac0becbf3855eaa93349c64e
DESKTOP-RQ8TUKQ\user <yoshizukiyo@gmail.co.kr>
2023-03-30 16:53:19 +0900

    NavLink , Link 링크기능 주석 추가

--- src/jsx_list/Header.jsx
+++ src/jsx_list/Header.jsx
@@ -4,16 +4,14 @@ import axios from 'axios'
 
 function Header() {
 	const [datas, setdatas] = useState([])
-	const nav_menu = ['메뉴1','메뉴2','메뉴3','메뉴4']
-	const test = useEffect(()=>{
+	
+	useEffect(()=>{
 		axios.get(`${process.env.PUBLIC_URL}/DB/members.json`).then((data)=>{
 			setdatas(data.data.members)
 		})
 	},[])
 	console.log(datas);
-	// useEffect(()=>{
-	// 	console.log(datas);
-	// },[])
+
 	return (
 		<>
 			<header>
@@ -22,7 +20,8 @@ function Header() {
 					{datas.map((data,idx)=>{
 						return (
 							<li key={idx}>
-								<NavLink to='/test' className='link'>{data.name}</NavLink>
+								<NavLink to='/test' className='link'>{data.name}</NavLink> {/* 탭기능 */}
+								<Link to='/test2'>{data.name}</Link> {/* 링크기능 */}
 							</li>
 						)
 					})}
