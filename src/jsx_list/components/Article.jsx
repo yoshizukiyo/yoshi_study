9ac3948c5385bce336a0d9bad73daf9a2c82e7f9
DESKTOP-RQ8TUKQ\user <yoshizukiyo@gmail.co.kr>
2023-04-20 16:14:39 +0900

    자식1 - 부모 - 자식2 데이터 전송 오류 해결

--- src/jsx_list/components/Article.jsx
+++ src/jsx_list/components/Article.jsx
@@ -1,8 +1,8 @@
 function Article({data}) {
-	console.log(data); 
+	console.log(data,'2'); 
 	return (
 		<>
-		{/* {'test'} */}
+		{/* {data} */}
 		<div className='content-box'>
 			<div className="box"></div>
 			<div className="box"></div>
--- src/jsx_list/main/Visual.jsx
+++ src/jsx_list/main/Visual.jsx
@@ -3,16 +3,17 @@ import { useRef,useState,useEffect } from "react";
 
 function Visual({setdata}) {
 	const content = useRef(null)
-	const [log, setlog] = useState()
+	// const [log, setlog] = useState()
 	useEffect(()=>{
-		setlog(content.current)
+		// content.current = [];
+		// setlog(content.current)
+		setdata(content.current)
 	},[])
-	setdata(log)
 
 	return (
 		<>
 		<article className='visual'>
-			<Link ref={content} to='/' onClick={()=>{test()}} >
+			<Link ref={content} to='/' >
 			<img src={`${process.env.PUBLIC_URL}/favicon.ico`} alt="리엑트 아이콘" />
 			</Link>
 		</article>
